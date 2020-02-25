c0ebdd7e47c4c2beefdfad3f9fbd3f4e2960d4a7
Nikhil CM <nikhil@innovatise.com>
2020-02-25 11:19:09 +0530

    fix: cant reuse function

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const {performance} = require('perf_hooks');
-function perf (mark) {
+function perf () {
+    const fn =  function(mark){
+        fn.obj = fn.obj || {};
+        fn._summary = fn._summary || {};
+        const time = performance.now();
+        if (!fn.obj[mark]) {
+            fn.obj[mark] = time;
+            return 0;
+        }
 
-    perf.obj = perf.obj || {};
-    perf._summary = perf._summary || {};
-    const time = performance.now();
-    if (!perf.obj[mark]) {
-        perf.obj[mark] = time;
-        return 0;
+        const timeTaken = (time - fn.obj[mark]).toFixed(2);
+        console.log(`PERF:${mark}:`, timeTaken);
+        fn._summary[mark] = timeTaken;
+        delete fn.obj[mark];
+        return timeTaken;
     }
-
-    const timeTaken = (time - perf.obj[mark]).toFixed(2);
-    console.log(`PERF:${mark}:`, timeTaken);
-    perf._summary[mark] = timeTaken;
-    delete perf.obj[mark];
-    return timeTaken;
+    return fn;
 }
 module.exports = perf;
