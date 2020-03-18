e3657f2138f7c1a14a824ef4dad34f7e75717ef0
Nikhil CM <nikhil@innovatise.com>
2020-03-18 22:12:45 +0530

    optionallu enable logging

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const {performance} = require('perf_hooks');
 const {PERF_LOG} = process.env;
 
-function perf() {
+function perf({enabled = true}) {
     const fn = function (mark) {
-        if ([undefined, null, false, "false"].includes(PERF_LOG)) {
-            return;
-        }
-        fn.obj = fn.obj || {};
-        fn._summary = fn._summary || {};
-        const time = performance.now();
-        if (!fn.obj[mark]) {
-            fn.obj[mark] = time;
-            return 0;
-        }
+        if ([undefined, null, false, "false"].includes(PERF_LOG) || enabled) {
+            fn.obj = fn.obj || {};
+            fn._summary = fn._summary || {};
+            const time = performance.now();
+            if (!fn.obj[mark]) {
+                fn.obj[mark] = time;
+                return 0;
+            }
 
-        const timeTaken = (time - fn.obj[mark]).toFixed(2);
-        console.log(`PERF:${mark}:`, timeTaken);
-        fn._summary[mark] = timeTaken;
-        delete fn.obj[mark];
-        return timeTaken;
+            const timeTaken = (time - fn.obj[mark]).toFixed(2);
+            console.log(`PERF:${mark}:`, timeTaken);
+            fn._summary[mark] = timeTaken;
+            delete fn.obj[mark];
+            return timeTaken;
+        }
     };
     return fn;
 }
